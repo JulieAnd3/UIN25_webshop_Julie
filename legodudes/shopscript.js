f6dd9d420348e473ae09672d02e648ee6ac7628e
JulieAnd3 <julia@hiof.no>
2025-01-20 11:54:42 +0100

    Ikke noe i js filen som er feil
    Alt som er skrevet i js filen shopscript er riktig som betyr at feilen ligger i en av de andre filene. Må undersøkes videre

--- legodudes/shopscript.js
+++ legodudes/shopscript.js
@@ -1,10 +1,8 @@
+
 console.log(products)
-
 //Gå gjennom alle produkter, generer HTML for hvert produkt, skrive til index.html 
-
 //En variabel som kan holde på HTML-en for produktene
 let productHTML = ""
-
 products.map((product, index) => productHTML += 
             `<article class="product-card">
                 <img src="website_images/PROD_${product.imagefile}" alt="PRODUKTTITTEL" />
@@ -13,22 +11,18 @@ products.map((product, index) => productHTML +=
                 <p>Kr. ${product.price},-</p>
                 <button onclick="addProductToCart(${product.prodid})">Legg til i handlekurv</button>
             </article>`)
-
 //Finn #productlist of fyll den med verdiene i variabelen productHTML
 document.getElementById("productlist").innerHTML = productHTML
-
 //Lage toggel funksjonalitet for handlevogn 
 document.getElementById("shoppingcart").addEventListener("click", function() {
     document.getElementById("cart").classList.toggle("visible") //classList sjekker hvilken klasse den her. Toggle er en innebygd if else test
 })
-
 //Lage addProductToCart()
 function addProductToCart(prodid){
     console.log("Du vil legge til produkt id: " + prodid)
     /*bruk .some for å sjekke om prodid allerde finnes i cart: some returner true eller false
     Sjekker om prodid some finner er allerede i parameteren prodid*/
     const idExists = cart.some(cartprod => cartprod.cartprodid === prodid) 
-
     if(idExists) { //noe funker ikke enda 
         //Oppdatere dette produktes quantity
         //Først finne indexen til denne id-en:
@@ -39,42 +33,33 @@ function addProductToCart(prodid){
         //Produktet er ikke handlevognen og vi må oppdatere det 
         cart.push({cartprodid: prodid, quantity: 1})
     }
-
     printCart()
     console.log(cart)
 }
-
 
 //Lage en funskjon som skriver oppdatert versjon av handlevognen
 function printCart(){
     //Starte med en tom variabel vi kan fylle med HTML
     let cartHTML = ""
-
     //Lag klar variabel for pris:
     let cartTotal = 0;
-
     //lage variabel for antall produkter:
     let cartNumber = 0;
-
-    //Gå gjennom cart-arrayen og generer HTML for hvert produkt 
-    cart.map((cartprod, index) => {
-        const currentProduct = products.findIndex(p => p.prodidv === cartprod.cartprodid)
-        const currentProductInfo = products[currentProduct]
-        cartHTML +=  //Feilmelding på title?
-        `       <article class="cart-product">
-                    <span class="title">${currentProductInfo.title}</span> 
-                    <span class="price">${currentProductInfo.price},-</span>
-                    <span class="quantity">x<span class="quantity-number">${cartprod.quantity}</span></span>
-                    <button class="delete">x</button>
-                </article>
-        `
-        //Regn ut totalsum:
-        cartTotal += currentProductInfo.price *cartprod.quantity
-
-        //Regn ut antall produkter 
-        cartNumber += cartprod.quantity
-    })
-
+ //Gå gjennom cart-arrayen og generere HTML for hvert produkt:
+ cart.map((cartprod, index) => {
+    const currentProduct = products.findIndex(p => p.prodid === cartprod.cartprodid)
+    const currentProductInfo = products[currentProduct]
+    cartHTML += `<article class="cart-product">
+                <span class="title">${currentProductInfo.title}</span>
+                <span class="price">${currentProductInfo.price},-</span>
+                <span class="quantity">x<span class="quantity-number">${cartprod.quantity}</span></span>
+                <button class="delete">x</button>
+            </article>`
+    //Regn ut totalsum:
+    cartTotal += currentProductInfo.price * cartprod.quantity
+    //Regn ut antall produkter:
+    cartNumber += cartprod.quantity
+})
     //Skrive ut generert HTML til indexfila:
     document.getElementById("cart-products").innerHTML = cartHTML 
     //Skrive ut totalpris:
@@ -82,5 +67,4 @@ function printCart(){
     //Skrive ut antall produkter:
     document.getElementById("cartcount").innerHTML = cartNumber
 }
-
 printCart()
